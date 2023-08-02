f19d1b16e23e51c39ffe0bfed46c875817cfbdf7
KSHDestiny <kaungsathein.5899@gmail.com>
2023-08-03 06:24:28 +0630

    Final

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import Output from "./components/Output";
 import { useState } from "react";
 
 function App() {
+  const [output, setOutputValue] = useState(0);
+  const [input, setInputValue] = useState(0);
 
-  const [ output, setOutputValue ] = useState(0);
-  const [ input, setInputValue ] = useState(0);
-
-  function clickEventHandling(value){
-
+  function clickEventHandling(value) {
     switch (value) {
       case "AC":
         setInputValue(0);
@@ -20,18 +18,20 @@ function App() {
         break;
 
       case "Del":
-        if(input.length > 0){
-          input.length == 1? setInputValue(0) : setInputValue(input.slice(0,input.length - 1));
+        if (input.length > 0) {
+          input.length == 1
+            ? setInputValue(0)
+            : setInputValue(input.slice(0, input.length - 1));
         }
         break;
 
       case "=":
-          setOutputValue(eval(input));
-          setInputValue(0);
+        setOutputValue(eval(input));
+        setInputValue(0);
         break;
-    
+
       default:
-        input == 0 ? setInputValue(value) : setInputValue(input+value);
+        input == 0 ? setInputValue(value) : setInputValue(input + value);
         break;
     }
   }
@@ -39,8 +39,8 @@ function App() {
   return (
     <div className="container">
       <div className="calculator-frame">
-        <Output output={output}/>
-        <Input input={input}/>
+        <Output output={output} />
+        <Input input={input} />
         <Button clickEventHandling={clickEventHandling} />
       </div>
     </div>
